61efb3e9afc614a1dd60fe811e8714058253c662
xxxboss <47444486+xxxboss@users.noreply.github.com>
2019-02-22 14:36:56 +0900

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<windows.h>
+#include<time.h>
+int main(){
+    int a{ },b=0;
 
+    do {
+        scanf("%d",&a);
+        b++;
+        printf("%d\n",a);
+    }while(a);
+
+}
