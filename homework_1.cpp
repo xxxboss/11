5fc1ac960cbd540be8bccfbe5a4cf2301557961a
xxxboss <47444486+xxxboss@users.noreply.github.com>
2019-02-22 15:12:45 +0900

    Update homework_1.cpp

--- homework_1.cpp
+++ homework_1.cpp
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<windows.h>
+#include<time.h>
+int main(){
+    int a,b,i;
+    scanf("%d",&a);
+    if (a%2==1){
+        a=a-1;
+    }
+    i=a;
+    for(i=a;i>=0;i-=2){
+
+        b+=i;
+
+    }
+    printf("%d",b);
+    return 0;
+}
 
